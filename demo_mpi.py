8cd788d5043f46fda790a9ca4a1a2074dbe3a7e3
SociuRoberto <117272324+SociuRoberto@users.noreply.github.com>
2023-04-13 11:34:39 +0300

    Update demo_mpi.py

--- demo_mpi.py
+++ demo_mpi.py
@@ -1,9 +1,13 @@
+#Folosim libreria mpi4py de la Python
 import mpi4py
 
+#Din libreria mpi4py, folosim o bucata care ne trebuie, in cazul asta MPI
 from mpi4py import MPI
 
+#Folosim comenzile "comm" sa preluam rank-ul si size-ul
 comm =  MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
+#Aicea tiparim ce vrem prin print
 print (f"Hello world from process {rank} out of {size} processes!")
