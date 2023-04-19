a9ee03b3cceab20e59c5edaeda3ba588dbf8d338
Valentin Cojocari <valentin21062000@yahoo.com>
2023-04-19 14:34:19 +0300

    Cod python care utilizeaza MPI

---
+++ lab6.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+from random import randint
+from mpi4py import MPI
+
+
+def throw_dice(n=1, prnt=False):
+    values = []
+    for i in range(n):
+        values.append(randint(1, 6))
+
+    if prnt:
+        print(values)
+
+    return values
+
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+if rank == 0:
+    dice_values = throw_dice(prnt=True)
+    comm.send(dice_values[0], 3)
+
+elif rank == 1:
+    dice_values = throw_dice(n=2, prnt=True)
+    comm.send(sum(dice_values), 3)
+
+elif rank == 2:
+    dice_values = throw_dice(n=3, prnt=True)
+    comm.send(sum(dice_values), 3)
+elif rank == 3:
+    received = []
+    for i in range(size):
+        if i == rank:
+            continue
+
+        received.append(comm.recv(source=i))
+    print(sum(received))
+
+
+print(f"Process {rank} of {size} ended")
