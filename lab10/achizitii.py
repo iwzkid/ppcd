e13cf66bbd95ff3b8894a32ddd326d0dc5435824
Lidia Matei <lia@xkid.ro>
2023-04-27 12:29:51 +0300

    Fixed indentation.

--- lab10/achizitii.py
+++ lab10/achizitii.py
@@ -8,8 +8,6 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-# <PSEUDOCODE>
-
 # Manager. // Rank 0.
 if rank == 0:
     #Let acquisitions know of the required q.
@@ -50,20 +48,11 @@ if rank in range(2, 5):
     amount = random.randint(quantity-50, quantity+50)
     price_per_unit = round(random.uniform(1, 5), 2) #RON
 
-
-
     offer = {
         'amount': amount,
         'price_per_unit': price_per_unit
     }
 
     comm.send(offer, 0)
-
-
-#Exemplu:
-#Oferta 1: (25, 1)
-#Oferta 2: (2000, 2)
-#Oferta 3: (50, 1.5)
-#Managerul alege combinaţia: 25 caiete la 1 leu + 50 caiete la 1.5 lei + 25 caiete la 2 lei, pret total: 25+75+50=150 lei
 
 # mpiexec -np 5 python .\lab10\achizitii.py
